4da3ba5f6330ee3ff17ba80bff7a154635eb24e2
AzamatKhemraev <aza200@mail.ru>
2021-08-23 12:41:00 +0300

    lesson 9

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
@@ -8,8 +8,25 @@ import HomePage from "../HomePage/HomePage";
 import Page404 from "../Page404/Page404";
 import Profile from "../Profile/Profile";
 import { persistor, store } from "../Store/Store";
+import { Login } from "../Login/Login";
+import firebase from "firebase";
+import PublicRoute from "../hocs/PublicRoute";
+import PrivateRoute from "../hocs/PrivateRoute";
+import { Signup } from "../Signup/Signup";
 
 export default function Routes(props) {
+  const [authed, setAuthed] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setAuthed(true);
+      } else {
+        setAuthed(false);
+      }
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
@@ -28,22 +45,35 @@ export default function Routes(props) {
               <li>
                 <Link to="/SW">Star wars</Link>
               </li>
+              <li>
+                <Link to="/Signup">Registration</Link>
+              </li>
+              <li>
+                <Link to="/Login">Login</Link>
+              </li>
             </ul>
           </nav>
           <Switch>
-            <Route exact path="/">
+            <PublicRoute authenticated={authed} exact path="/">
               <HomePage />
-            </Route>
-            <Route path="/Profile">
+            </PublicRoute>
+            <PublicRoute authenticated={authed} path="/Login">
+              <Login />
+            </PublicRoute>
+            <PublicRoute authenticated={authed} path="/Signup">
+              <Signup />
+            </PublicRoute>
+            <PrivateRoute authenticated={authed} path="/Profile">
               <Profile />
-            </Route>
-            <Route
+            </PrivateRoute>
+            <PrivateRoute
+              authenticated={authed}
               path="/Chats/:chatId?"
               render={(data) => <ChatsList match={data.match} />}
-            ></Route>
-            <Route path="/SW">
+            ></PrivateRoute>
+            <PublicRoute authenticated={authed} path="/SW">
               <SW />
-            </Route>
+            </PublicRoute>
             <Route path="*">
               <Page404 />
             </Route>
---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDa8jvv3vI1zL_W_hAonPpEncAyJQN-FOg",
+  authDomain: "gb-react-ak.firebaseapp.com",
+  databaseURL: "https://gb-react-ak-default-rtdb.firebaseio.com",
+  projectId: "gb-react-ak",
+  storageBucket: "gb-react-ak.appspot.com",
+  messagingSenderId: "784132985484",
+  appId: "1:784132985484:web:660dce861268a8b9efe1a9",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
